9a5972135d69fff704c14cd61d8cd08518750807
Damir Pavlik <damir.pavlik@gmail.com>
2024-01-20 21:31:34 +0100

    style: styling the 404 page template

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,16 @@
+import { Heading } from "../../components";
+
 const NotFound = () => {
-  return <div>404</div>;
+  return (
+    <div className="flex justify-center flex-col items-center h-screen">
+      <Heading
+        fontSize="lg:text-[11vw] text-[19vw]"
+        headingText="404"
+        additionalStyles="leading-none !mb-0"
+      />
+      <p>Page Not Found</p>
+    </div>
+  );
 };
 
 export default NotFound;
