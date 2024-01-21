e1fc96893dd357c2f14bb7ebdee881c9e32c230a
Damir Pavlik <damir.pavlik@gmail.com>
2024-01-21 19:05:02 +0100

    style: minor style changes

--- src/pages/Experience/ExperienceTabs.tsx
+++ src/pages/Experience/ExperienceTabs.tsx
@@ -7,12 +7,12 @@ const ExperienceTabs: React.FC<ExperienceTabsProps> = ({
   return (
     <div className="lg:col-span-2 col-span-12">
       <div className="flex lg:flex-row flex-col">
-        <div className="lg:flex-grow lg:block flex justify-between">
+        <div className="lg:flex-grow lg:block flex justify-around">
           <h4
             onClick={() => handleTabClick(1)}
             className={`${
               activeTab === 1 ? "underline" : "text-gray"
-            } cursor-pointer lg:text-xl text-[18px] lg:mb-0 mb-3`}
+            } cursor-pointer lg:text-xl text-[18px] lg:mb-6 mb-3`}
           >
             Work Experience
           </h4>
@@ -20,12 +20,12 @@ const ExperienceTabs: React.FC<ExperienceTabsProps> = ({
             onClick={() => handleTabClick(2)}
             className={`${
               activeTab === 2 ? "underline" : "text-gray"
-            } cursor-pointer lg:text-xl text-[18px] lg:mb-0 mb-3 `}
+            } cursor-pointer lg:text-xl text-[18px] lg:mb-0 mb-3 lg:mr-0 mr-7`}
           >
             Projects
           </h4>
         </div>
-        <div className="lg:self-end lg:w-[1px] w-full lg:h-[400px] h-[1px] lg:bg-brown bg-gray lg:mb-0 mb-8"></div>
+        <div className="lg:self-end lg:w-[1px] w-[100vw] lg:h-[400px] h-[1px] lg:bg-brown bg-gray lg:mb-0 mb-8 lg:mx-0 -mx-[20px]"></div>
       </div>
     </div>
   );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,8 +3,8 @@ import Navigation from "./Navigation";
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-12  h-screen lg:items-center">
-      <div className="col-span-12 lg:col-span-4 mt-16 lg:mt-[90%] lg:-ml-[30px]">
+    <div className="grid grid-cols-12  lg:h-screen lg:items-center">
+      <div className="col-span-12 lg:col-span-4 mt-16 lg:mt-[90%] lg:-ml-[30px] lg:mb-0 mb-12">
         <Hero />
       </div>
       <div className="col-span-12 lg:col-span-8">
