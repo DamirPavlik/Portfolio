3253cd0b86ca4a5aff0febbed4af5bbe081771de
Damir Pavlik <damir.pavlik@gmail.com>
2024-01-21 19:04:00 +0100

    refactor: making the months of experience dynamic

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -2,6 +2,12 @@ import { Heading, Subheading } from "../../components";
 import { Link } from "react-router-dom";
 
 const About = () => {
+  const currentDate = new Date();
+  const startedDate = new Date(2023, 5, 1);
+  let monthsOfWorking =
+    (currentDate.getFullYear() - startedDate.getFullYear()) * 12 +
+    currentDate.getMonth() -
+    startedDate.getMonth();
   return (
     <div className="pt-12 pb-8 lg:pb-24 lg:pt-5 ">
       <Heading
@@ -13,27 +19,27 @@ const About = () => {
         <div className="col-span-12 lg:col-span-5">
           <div className="lg:mb-16 mb-12">
             <Subheading text="Hi there!" />
-            <p className="text-base text-gray mb-3">
+            <p className="lg:text-base text-sm text-gray mb-3">
               I am a 19-year-old web developer, specializing primarily in
               front-end technologies. With a solid start established at the age
               of 15, I embarked on a self-taught journey that led me to
-              successfully land my first client at the age of 17. Now, with 7
-              months of industry experience, I have honed my skills in
-              developing JavaScript-based applications, complemented by
-              occasional forays into backend development using PHP.
+              successfully land my first client at the age of 17. Now, with{" "}
+              {monthsOfWorking} months of industry experience, I have honed my
+              skills in developing JavaScript-based applications, complemented
+              by occasional forays into backend development using PHP.
             </p>
-            <p className="text-base text-gray mb-3">
+            <p className="lg:text-base text-sm text-gray mb-3">
               The inherent diversity embedded in each project is a source of
               excitement for me, and I actively seek and incorporate feedback
               from experienced colleagues to continually enhance my proficiency.
             </p>
-            <p className="text-base text-gray mb-8">
+            <p className="lg:text-base text-sm text-gray mb-8">
               My journey of continual improvement is a testament to my
               dedication to personal and professional development.
             </p>
             <a
               href="#"
-              className="bg-brown text-beige rounded-[4px] py-[10px] px-[20px] text-base"
+              className="bg-brown text-beige rounded-[4px] py-[10px] px-[20px] lg:text-base text-sm"
             >
               Resume
             </a>
@@ -41,7 +47,7 @@ const About = () => {
 
           <div className="lg:mb-16 mb-12">
             <Subheading text="Why me?" />
-            <p className="text-base text-gray mb-3">
+            <p className="lg:text-base text-sm text-gray mb-3">
               I am a web developer with a profound passion for my craft and I've
               got the skills to turn your ideas into reality. Whether your
               projects require specialized coding or design, I’ve got you
@@ -50,14 +56,14 @@ const About = () => {
               apart is my unwavering dedication to maintaining the highest
               standards.{" "}
             </p>
-            <p className="text-base text-gray mb-3">
+            <p className="lg:text-base text-sm text-gray mb-3">
               If you're looking for a developer who's all about speed and
               cutting corners, we are probably not the right fit. If, however,
               you want to work with someone who’s been around the block a few
               times and consistently delivers on promises, then we are indeed
               the right fit.{" "}
             </p>
-            <p className="text-base text-gray mb-8">
+            <p className="lg:text-base text-sm text-gray mb-8">
               Your satisfaction is not just a goal but a commitment, and I am
               here to ensure the success of your endeavors. If this aligns with
               your vision, please feel free to leave me a message, and let's
@@ -65,7 +71,7 @@ const About = () => {
             </p>
             <Link
               to="/contact"
-              className="bg-brown text-beige rounded-[4px] py-[10px] px-[20px] text-base"
+              className="bg-brown text-beige rounded-[4px] py-[10px] px-[20px] lg:text-base text-sm"
             >
               Get in touch
             </Link>
@@ -73,12 +79,12 @@ const About = () => {
 
           <div>
             <Subheading text="Skills" />
-            <p className="text-base text-gray ">
+            <p className="lg:text-base text-sm text-gray ">
               Some of the technologies I’ve used:
             </p>
-            <p className="text-base text-gray">
-              React, JavaScript, TypeScript, PHP, MySQL, WordPress, Sass,
-              Bootstrap, TailwindCSS, Git, Figma, Photoshop
+            <p className="lg:text-base text-sm text-gray">
+              React, JavaScript, TypeScript, jQuery, PHP, MySQL, WordPress,
+              Sass, Bootstrap, TailwindCSS, Git, Figma, Photoshop
             </p>
           </div>
         </div>
