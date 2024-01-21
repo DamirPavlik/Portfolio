5ffc8a44ea365c6e8687b84806f351fbdf15f95d
Damir Pavlik <damir.pavlik@gmail.com>
2024-01-21 19:01:27 +0100

    style: changing the hover animation on the homepage nav

--- src/pages/Home/Navigation.tsx
+++ src/pages/Home/Navigation.tsx
@@ -5,21 +5,21 @@ const Navigation = () => {
     <>
       <Link
         to="/experience"
-        className="text-[15vw] lg:text-[10vw] block font-grandslang text-brown nav-item"
+        className="text-[15vw] lg:text-[10vw] block font-grandslang text-brown hover:ml-3 transition-all duration-300"
       >
         Experience
       </Link>
       <hr className="bg-brown -mx-5 lg:mx-0" />
       <Link
         to="/about"
-        className="text-[15vw] lg:text-[10vw] block font-grandslang text-brown nav-item"
+        className="text-[15vw] lg:text-[10vw] block font-grandslang text-brown hover:ml-3 transition-all duration-300"
       >
         About
       </Link>
       <hr className="bg-brown -mx-5 lg:mx-0" />
       <Link
         to="/contact"
-        className="text-[15vw] lg:text-[10vw] block font-grandslang text-brown nav-item"
+        className="text-[15vw] lg:text-[10vw] block font-grandslang text-brown hover:ml-3 transition-all duration-300"
       >
         Contact
       </Link>
