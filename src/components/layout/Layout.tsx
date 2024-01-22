c8303b886e1317afd8e0082bc992e2e8ab92d318
Damir Pavlik <damir.pavlik@gmail.com>
2024-01-23 00:03:03 +0100

    refactor: sharing loading state through context

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,26 @@
 import { Outlet, useLocation } from "react-router-dom";
 import { Sidebar } from "..";
 import Loading from "./Loading";
+import { createContext, useEffect, useState } from "react";
+
+export const LoadingContext = createContext<number>(0);
 
 const Layout = () => {
   const isHomePage = useLocation().pathname === "/";
+  const [loading, setLoading] = useState<number>(0);
+
+  useEffect(() => {
+    const loadingNum = setTimeout(() => {
+      setLoading((prevState) => (prevState < 100 ? prevState + 1 : prevState));
+    }, 30);
+
+    return () => {
+      clearTimeout(loadingNum);
+    };
+  }, [loading]);
+
   return (
-    <>
+    <LoadingContext.Provider value={loading}>
       <div className={`bg-beige ${isHomePage ? "h-screen" : ""}`}>
         <Loading />
         <div className="grid grid-cols-12 mx-5 lg:mx-[50px] ">
@@ -17,7 +32,7 @@ const Layout = () => {
           </div>
         </div>
       </div>
-    </>
+    </LoadingContext.Provider>
   );
 };
 
--- src/components/layout/Loading.tsx
+++ src/components/layout/Loading.tsx
@@ -1,27 +1,23 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
+import { LoadingContext } from "./Layout";
 
 const Loading = () => {
-  const [loading, setLoading] = useState<number>(0);
-
-  useEffect(() => {
-    let loadingNum = setTimeout(() => {
-      setLoading((prevState: any) => prevState + 1);
-    }, 30);
-    if (loading === 100) {
-      clearTimeout(loadingNum);
-    }
-  }, [loading]);
+  const loading = useContext(LoadingContext);
 
   return (
-    <div
-      className={`fixed w-full h-screen bg-brown z-50 ${
-        loading === 100 ? "translate-y-[-100%] transition-all duration-700" : ""
-      }`}
-    >
-      <h1 className="absolute font-grandslang lg:text-[15vw] text-[20vw] text-beige lg:right-20 lg:bottom-10 lg:left-auto left-1/2 lg:top-auto top-1/2 lg:translate-x-[none] -translate-x-1/2 lg:translate-y-[none] -translate-y-1/2 leading-none">
-        {loading}%
-      </h1>
-    </div>
+    <>
+      <div
+        className={`fixed w-full h-screen bg-brown z-50 ${
+          loading === 100
+            ? "translate-y-[-100%] transition-all duration-700"
+            : ""
+        }`}
+      >
+        <h1 className="absolute font-grandslang lg:text-[15vw] text-[20vw] text-beige lg:right-20 lg:bottom-10 lg:left-auto left-1/2 lg:top-auto top-1/2 lg:translate-x-[none] -translate-x-1/2 lg:translate-y-[none] -translate-y-1/2 leading-none">
+          {loading}%
+        </h1>
+      </div>
+    </>
   );
 };
 
