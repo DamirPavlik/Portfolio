e2e21bba0da594f280408dd0065abbc57cdad10a
Damir Pavlik <damir.pavlik@gmail.com>
2024-01-23 00:01:34 +0100

    refactor: storing utils into a index file for easier exporting

---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+import { checkFieldLength } from "./checkFieldLength";
+import { isValidEmail } from "./isValidEmail";
+import { sendEmail } from "./sendEmail";
+
+export {
+    checkFieldLength,
+    isValidEmail,
+    sendEmail
+}
