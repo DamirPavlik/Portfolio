60e60f9f1a1dcbc3e3b1283c131296b42ded1732
Damir Pavlik <damir.pavlik@gmail.com>
2024-01-24 23:21:39 +0100

    refactor: making the headings have a typing effect

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -1,15 +1,25 @@
+import { useContext } from "react";
 import { HeadingProps } from "../../types/types";
+import { ReactTyped } from "react-typed";
+import { LoadingContext } from "../layout/Layout";
 
 const Heading: React.FC<HeadingProps> = ({
   headingText,
   fontSize,
   additionalStyles,
 }) => {
+  const loading = useContext(LoadingContext);
   return (
     <h2
       className={`${fontSize} font-grandslang text-brown mb-6 lg:mb-7 relative ${additionalStyles}`}
     >
-      {headingText}
+      {loading === 100 && (
+        <ReactTyped
+          className="font-grandslang"
+          strings={[headingText]}
+          typeSpeed={80}
+        />
+      )}
     </h2>
   );
 };
